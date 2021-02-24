0c01cbfccff09f3ea47a753687d0ba9d2fe9fe72
HannaBM12 <Hannabmulugeta@gmail.com>
2021-02-24 15:59:29 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  function taskItem(task){
+      let item1 = document.querySelector('#tasks')
+      let listOne = document.createElement('li')
+      listOne.innerText = task
+
+      let button = document.createElement('button')
+      button.innerText = "X"
+      button.className = "cancelButton"
+
+      listOne.appendChild(button)
+
+      item1.appendChild(listOne)
+  }
+  //  taskItem()
+
+    function formSubmit(){
+    let taskForm = document.querySelector('#create-task-form')
+        taskForm.addEventListener('submit', function(event){
+          event.preventDefault()
+        
+          let description = event.target.items.value
+          taskItem(description)
+          taskForm.reset() 
+          removeItem()
+        })
+ 
+   }
+
+   function removeItem(){
+        let buttons = document.querySelectorAll(".cancelButton")
+          console.log(buttons)
+          buttons.forEach(item => {
+            item.addEventListener('click', event => {
+               item.parentElement.remove()
+            })
+          })
+   }
+   formSubmit()
+  
+
+  
 });
+
+
